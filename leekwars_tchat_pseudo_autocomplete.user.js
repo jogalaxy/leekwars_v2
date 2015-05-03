5776f4df342c6c466f1105fcd4002b8886da5af4
jogalaxy <jo123456789@gmx.fr>
2015-05-03 10:12:54 +0200

    first commit

---
+++ leekwars_tchat_pseudo_autocomplete.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name			[Leek Wars] Tchat Pseudo Autocomplete
+// @namespace		https://github.com/jogalaxy/leekwars_v2
+// @version			0.1
+// @description		Ajout de l'autocomplétion pour les pseudos dans le tchat
+// @author			jojo123
+// @projectPage		https://github.com/jogalaxy/leekwars_v2
+// @downloadURL		https://github.com/jogalaxy/leekwars_v2/raw/master/leekwars_tchat_pseudo_autocomplete.user.js
+// @updateURL		https://github.com/jogalaxy/leekwars_v2/raw/master/leekwars_tchat_pseudo_autocomplete.user.js
+// @match			http://beta.leekwars.com/*
+// @grant			none
+// ==/UserScript==
