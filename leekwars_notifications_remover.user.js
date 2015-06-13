fa81f35685e377348fc8b7797ca4ef2a4e08690d
Rominou <befire@hotmail.fr>
2015-06-13 22:41:24 +0200

    [Fix] Version

--- leekwars_notifications_remover.user.js
+++ leekwars_notifications_remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          [Leek Wars] Notifications Remover
 // @namespace     https://github.com/jogalaxy/leekwars_v2
-// @version       0.4
+// @version       0.4.1
 // @description   Permet de supprimer la bulle des notifications
 // @author        Rominou & jojo123 & Keorl
 // @projectPage   https://github.com/jogalaxy/leekwars_v2
