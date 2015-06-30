a30d480c40d67c9ff731f73ea179060966f556c7
jogalaxy <jo123456789@gmx.fr>
2015-06-30 12:23:20 +0200

    update

--- leekwars_private_message_alert.user.js
+++ leekwars_private_message_alert.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          [Leek Wars] Private Message Alert
 // @namespace     https://github.com/jogalaxy/leekwars_v2
-// @version       0.1
+// @version       0.2
 // @description   Affiche une popup lors de la réception d'un message privé
 // @author        jojo123
 // @projectPage   https://github.com/jogalaxy/leekwars_v2
@@ -12,6 +12,9 @@
 // ==/UserScript==
 
 _.view.loaded['main.private_message_popup'] = "<div class='title'>Message privé</div><div class='content'>Vous avez reçu un nouveau message privé de <b>{{ name }}</b> !</div><div class='actions'><div class='action dismiss'>Ok !</div></div>";
+
+var count_popup = 0;
+var _popup = null;
 
 function init_socket()
 {
@@ -24,6 +27,8 @@ function init_socket()
 			var _msg = JSON.parse(msg.data);
 			if (_msg[0] == MP_RECEIVE)
 				popup(_msg[1][2]);
+			if (_msg[0] == MP_UNREAD_MESSAGES && _msg[1][0] == 0 && _popup)
+				_popup.dismiss();
 		}
 		clearInterval(interval_init_socket);
 	}
@@ -33,6 +38,19 @@ var interval_init_socket = setInterval(init_socket, 100);
 
 function popup(farmer_name)
 {
-	var popup = new _.popup.new('main.private_message_popup', {name: farmer_name}, 900, true);
-	popup.show();
+	console.log("count_popup", count_popup);
+
+	if (count_popup == 0 && LW.currentPage != "messages")
+	{
+		_popup = new _.popup.new('main.private_message_popup', {name: farmer_name}, 900, true);
+
+		_popup.setOnDismiss(function()
+		{
+			count_popup = 0;
+		});
+
+		count_popup = 1;
+
+		_popup.show();
+	}
 }
