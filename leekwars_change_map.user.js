60781d8f39aa54aba89a4593c2984d73cc337d1b
jogalaxy <jo123456789@gmx.fr>
2015-08-21 17:51:07 +0200

    udapte

---
+++ leekwars_change_map.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name          [Leek Wars] Change MAP
+// @namespace     https://github.com/jogalaxy/leekwars_v2
+// @version       0.1
+// @description   Permet de mettre sur tous les combats la map d'entrainement toute blanche
+// @author        jojo123
+// @projectPage   https://github.com/jogalaxy/leekwars_v2
+// @downloadURL   https://github.com/jogalaxy/leekwars_v2/raw/master/leekwars_change_map.user.js
+// @updateURL     https://github.com/jogalaxy/leekwars_v2/raw/master/leekwars_change_map.user.js
+// @match         http://leekwars.com/*
+// @grant         none
+// ==/UserScript==
+
+LW.on('pageload', function()
+{
+	if (LW.currentPage == 'fight')
+	{
+		var initInt = setInterval(function()
+		{
+			if (typeof M != "undefined")
+			{
+				game.map = M[0];
+				game.ground.resize();
+				LW.pages.fight.resize();
+				clearInterval(initInt);
+			}
+		}, 100);
+	}
+});
