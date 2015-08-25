6ec1a37f4f865a219899c3364b4d442ca27ac065
jogalaxy <jo123456789@gmx.fr>
2015-08-25 16:59:48 +0200

    update

---
+++ multiaccounts_db.js
@@ -0,0 +1,51 @@
+
+/**
+ * Multi Accounts Database
+ */
+
+localStorage['multiaccount_db'] = JSON.stringify({
+
+	'jojo123' : [
+		'ONeill',
+		'leekidator1',
+		'service001'
+	],
+
+	'WhiteSlash' : [
+		'KounT',
+		'thibault',
+		'ts23',
+		'BlackSlash'
+	],
+
+	'srax' : [
+		'Hendricks'
+	],
+
+	'AlucardDH' : [
+		'hurlutipopogroum'
+	],
+
+	'Silosis' : [
+		'SiloBall',
+		'SiloLeek',
+		'SiloSiloSiloSiloSiloSiloSiloSi',
+		'SilosisIsGenious',
+		'SilosisTM'
+	],
+
+	'Icallhimfess' : [
+		'chabrogar'
+	],
+
+	'Liemmerle' : [
+		'Lia'
+	],
+
+	'Twilight' : [
+		'Rhapsody',
+		'Sareel'
+	]
+
+
+});
