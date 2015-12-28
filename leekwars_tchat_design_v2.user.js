c3612b05aa49867cd5a400d25438275443a0cf31
jogalaxy <jo123456789@gmx.fr>
2015-12-28 10:42:48 +0100

    update

---
+++ leekwars_tchat_design_v2.user.js
@@ -0,0 +1,106 @@
+// ==UserScript==
+// @name          [Leek Wars] Tchat Design v2
+// @namespace     https://github.com/jogalaxy/leekwars_v2
+// @version       0.1
+// @description   Change le design du tchat
+// @author        jojo123
+// @projectPage   https://github.com/jogalaxy/leekwars_v2
+// @downloadURL   https://github.com/jogalaxy/leekwars_v2/raw/master/leekwars_tchat_design_v2.user.js
+// @updateURL     https://github.com/jogalaxy/leekwars_v2/raw/master/leekwars_tchat_design_v2.user.js
+// @match         http://leekwars.com/*
+// @grant         none
+// ==/UserScript==
+
+(function()
+{
+
+	var _init = false;
+
+	LW.on('pageload', function()
+	{
+		if (LW.farmer.id)
+		{
+			_init = true;
+			init(LW.farmer.id);
+		}
+	});
+
+	function init(farmer_id)
+	{
+		addGlobalStyle('\
+			#chat\
+			{\
+				background-color: rgb(50, 50, 50);\
+			}\
+			#chat-messages\
+			{\
+				background-color: rgb(30, 30, 30);\
+			}\
+			#chat-messages .chat-message\
+			{\
+				margin: 2px 0;\
+				padding: 5px 10px;\
+				max-width: 80%;\
+				border-radius: 5px;\
+				clear: both;\
+			}\
+			#chat-messages .chat-message-time\
+			{\
+				top: auto;\
+				bottom: 5px;\
+			}\
+			#chat-messages .chat-message-messages\
+			{\
+				padding-left: 0px;\
+			}\
+			#chat-messages .chat-avatar\
+			{\
+				border-radius: 40px;\
+				box-shadow: 0px 0px 10px rgb(90, 90, 90);\
+			}\
+			#chat-messages .chat-message\
+			{\
+				float:left;\
+				background-color: rgb(40, 40, 40);\
+				color: #d7d7d7;\
+				margin-left: 60px;\
+				border: 0;\
+			}\
+			#chat-messages .chat-message .chat-avatar\
+			{\
+				margin-left: -60px;\
+			}\
+			#chat-messages .chat-message[author="'+farmer_id+'"]\
+			{\
+				float:right;\
+				background-color: rgb(60, 60, 60);\
+				margin-right: 60px;\
+			}\
+			#chat-messages .chat-message[author="'+farmer_id+'"] .chat-avatar\
+			{\
+				float:right;\
+				margin-right: -60px;\
+			}\
+			#chat-messages .chat-message[author="'+farmer_id+'"] .chat-message-author\
+			{\
+				text-align: right;\
+			}\
+			#chat-input\
+			{\
+				background-color: #f0f0f0;\
+			}\
+		');
+	}
+
+	function addGlobalStyle(css)
+	{
+		var head, style;
+		head = document.getElementsByTagName('head')[0];
+		if (!head) { return; }
+		style = document.createElement('style');
+		style.type = 'text/css';
+		style.innerHTML = css;
+		head.appendChild(style);
+	}
+
+})();
