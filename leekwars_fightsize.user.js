20a62690cf067746954bc4d6a5f48638dbcd5974
LN <ln@test.com>
2016-03-19 13:20:57 +0100

    Resize controls option

--- leekwars_fightsize.user.js
+++ leekwars_fightsize.user.js
@@ -4,6 +4,9 @@
 // @version      0.1
 // @description  Resize the fight screen
 // @author       k-artorias
+// @projectPage   https://github.com/jogalaxy/leekwars_v2
+// @downloadURL   https://raw.githubusercontent.com/k-artorias/leekwars_v2/master/leekwars_fightsize.user.js
+// @updateURL     https://raw.githubusercontent.com/k-artorias/leekwars_v2/master/leekwars_fightsize.user.js
 // @match        http://leekwars.com/*
 // @grant        none
 // ==/UserScript==
@@ -15,34 +18,63 @@ function main () {
 
         var size = localStorage['fight_screen_size'] === undefined ? 900 : localStorage['fight_screen_size'];
 
+		var allResize = localStorage['fight_screen_resizeall'] === undefined ? false : (localStorage['fight_screen_resizeall']  === 'true');
+
         if (LW.currentPage == 'settings') {
 
             $('body').on('click', '#fight_screen_size_apply', function () {
             	var fight_screen_size = $('#fight_screen_size').val();
             	localStorage['fight_screen_size'] = fight_screen_size;
+				var fight_screen_resizeall = $('#fight_screen_resizeall').is(':checked');
+            	localStorage['fight_screen_resizeall'] = fight_screen_resizeall;
             	_.toast('Paramètres sauvegardés !');
             });
 
-		    $('#settings-page .flex-container').first().append('<div class="column6"><div class="panel"><div class="header"><h2>Taille écran de combat</h2></div><div class="content"><input type="text" id="fight_screen_size"><br><br><br><center><input class="button green" value="Appliquer" id="fight_screen_size_apply"></center></div></div></div>');
+		    $('#settings-page .flex-container').first().append('<div class="column6"><div class="panel"><div class="header"><h2>Taille écran de combat</h2></div><div class="content">Taille: <input type="text" id="fight_screen_size"><br><br>Tout redimensionner : <input type="checkbox" id="fight_screen_resizeall"><br><br><br><center><input class="button green" value="Appliquer" id="fight_screen_size_apply"></center></div></div></div>');
 
 		    $('#fight_screen_size').val(size);
+			if (allResize) {
+		    	$('#fight_screen_resizeall').prop('checked', true);
+			} else {
+				$('#fight_screen_resizeall').prop('checked', false);
+			}
 	    }
 
         if (LW.currentPage == 'fight') {
 
 		  LW.pages.fight.resize = function() {
 
+			if (allResize) {
+				$("#fight-page").css("width", size)
+				$("#fight-page").css("height", Math.ceil(game.width / RATIO))
+
+				$("#fight-page").css("margin-right", 'auto')
+				$("#fight-page").css("margin-left", 'auto')
+			}
+
 			if (game) {
 
                 $('#controls').removeClass('large')
 
-                game.width = size;
-                game.height = Math.ceil(game.width / RATIO)
+				if (allResize) {
+					game.width = $('#fight-page').width() - 15
+					game.height = Math.ceil(game.width / RATIO)
+				} else {
+					game.width = size;
+					game.height = Math.ceil(game.width / RATIO)
+
+					$("#bg-canvas").css("margin-right", 'auto')
+	                $("#bg-canvas").css("margin-left", 'auto')
+
+					$("#game-canvas").css("margin-right", 'auto')
+	                $("#game-canvas").css("margin-left", 'auto')
+				}
 
                 $("#fight").css("height", game.height)
                 $("#game").css("width", 'auto')
                 $("#game").css("height", 'auto')
-
+				$("#game").css("margin-right", 'auto')
+                $("#game").css("margin-left", 'auto')
 
 				$(canvas).attr("width", game.width)
 				$(canvas).attr("height", game.height)
