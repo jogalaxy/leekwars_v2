1032d3a33726c54a4948e63d6d746f0c9d5a185a
Antoine Neveux <antoine.neveux@gmail.com>
2017-01-23 17:37:22 +0100

    Updated the import/export for managing import

--- leekwars_editor_import_export.user.js
+++ leekwars_editor_import_export.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name          [Leek Wars] Editor - Import & Export
 // @namespace     https://github.com/jogalaxy/leekwars_v2
-// @version       0.5
+// @version       0.6
 // @description   Permet d'importer et d'exporter ses IA
-// @author        jojo123 && caragane
+// @author        jojo123 && caragane && antoine
 // @projectPage   https://github.com/jogalaxy/leekwars_v2
 // @downloadURL   https://github.com/jogalaxy/leekwars_v2/raw/master/leekwars_editor_import_export.user.js
 // @updateURL     https://github.com/jogalaxy/leekwars_v2/raw/master/leekwars_editor_import_export.user.js
@@ -15,22 +15,85 @@
 LW.on('pageload', function()
 {
     if (LW.currentPage == 'editor')
-        {
-            $('#editor-page .tabs').prepend('<div id="export-button" class="tab">Export All</div>');
-            $('#export-button').click(export_all_functions);
-            $('#editor-page .tabs').prepend('<div id="export-button" class="tab">Export</div>');
-            $('#export-button').click(export_function);
-            $('#editor-page .tabs').prepend('<div id="uploader" class="tab" title="">Import</div>');
-            $('#editor-page .tabs').prepend('<input type="file" multiple id="importer" style="display:none;"/>');
-            $('#uploader').click(function(){$('#importer').click();});
-            $('#importer').on('change', importFiles);
-        }
-    });
+    {
+        $('#editor-page .tabs').prepend('<div id="export-button" class="tab">Export All</div>');
+        $('#export-button').click(export_all_functions);
+        $('#editor-page .tabs').prepend('<div id="export-button" class="tab">Export</div>');
+        $('#export-button').click(export_function);
+        $('#editor-page .tabs').prepend('<div id="uploader" class="tab" title="">Import</div>');
+        $('#editor-page .tabs').prepend('<input type="file" multiple id="importer" style="display:none;"/>');
+        $('#uploader').click(function(){$('#importer').click();});
+        $('#importer').on('change', importFiles);
+    }
+});
 
 function importFiles(event) {
-    _.toast("Implementation in progress...");
-    $.each(e.target.files, function(i, file) {
-        console.debug("Processing file: "+file.name);
+
+    var secured = confirm("Ask validation for each and every file?");
+
+    $.each(event.target.files, function(i, file) {
+        var name = file.name;
+        var reader = new FileReader();
+        reader.onload = function(e) { 
+            var content = e.target.result;
+            if (content != null) {
+                var occurences = 0;
+                for (var i in editors)
+                    if(editors[i].name == name) occurences ++;
+
+                    if(occurences == 0) {
+                        $.ajax({
+                            url: "https://leekwars.com/api/ai/new",
+                            type: "POST",
+                            dataType: "json",
+                            async: false,
+                            data: {folder_id: "0", token: "$"},
+                            success: function(result) {
+                                var ai = result.ai;
+                                $.ajax({
+                                    url: "https://leekwars.com/api/ai/rename",
+                                    type: "POST",
+                                    dataType: "json",
+                                    async: false,
+                                    data: {ai_id: ai.id, new_name: name, token: "$"},
+                                    success: function(result) {
+                                        $.ajax({
+                                            url: "https://leekwars.com/api/ai/save",
+                                            type: "POST",
+                                            dataType: "json",
+                                            async: false,
+                                            data: {ai_id: ai.id, code: content, token: "$"},
+                                            success: function(result) {
+                                                _.toast(name+" has been created!");
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    } else if (occurences == 1) {
+                        var current = null;
+                        for (var i in editors)
+                            if(editors[i].name == name) current = i;
+
+                        if (!secured || secured && confirm('Are you sure you want to override '+name+'?')) {
+                            editors[current].editor.setValue(content);
+                            editors[current].save();
+                            editors[current].loaded = false;
+                            editors[current].show();
+
+                            _.toast(name+" has been modified!");
+                        } else {
+                            _.toast(name+" has been bypassed!");
+                        }
+
+                    } else {
+                        _.toast(name+" already exists multiple times. Not doing anything.");
+                    }                
+
+                } else _.toast(name+" is empty and cannot be imported");
+            };
+            reader.readAsText(file);
     });
 }
 
@@ -97,70 +160,3 @@ function export_all_functions()
         a.remove(); 
     });
 }
-
-/*
-function upload_file(file)
-{
-var reader = new FileReader();
-reader.onload = function(e)
-{
-var content = e.target.result;
-var name = file.name;
-
-var alreadyExist = 0;
-for (var i in editors)
-if (editors[i].name == name) alreadyExist++;
-
-if (alreadyExist == 0)
-{
-_.post('ai/new', {}, function(data)
-{
-if (data.success)
-{
-var ai = data.ai;
-current = ai.id;
-_.post('ai/save/', {ai_id: current, code: content}, function(data)
-{
-editors[ai.id] = new Editor(ai.id, ai.name, true, ai.level, ai.code);
-$('.CodeMirror').css('font-size', _fontSize);
-editors[current].editor.setValue(content);
-editors[current].updateName(name);
-editors[current].save();
-editors[current].loaded = false;
-editors[current].show();
-_.toast('IA : "'+name+'" importée !');
-});
-}
-else
-_.toast('Erreur serveur !');
-});
-}
-else if (alreadyExist == 1)
-{
-var current = null;
-for (var i in editors)
-if (editors[i].name == name) current = i;
-
-var importPopup = new _.popup.new('editor.import_popup', {ai: name});
-importPopup.find('#import').click(function()
-{
-_.post('ai/save/', {ai_id: current, code: content}, function(data)
-{
-editors[current].editor.setValue(content);
-editors[current].save();
-editors[current].loaded = false;
-editors[current].show();
-_.toast('IA : "'+name+'" importée !');
-});
-importPopup.dismiss();
-});
-importPopup.show(e)
-}
-else
-{
-_.toast('Erreur ! Plusieures IA sont nommée : "'+name+'"');
-}
-}
-reader.readAsText(file);
-}
-*/
